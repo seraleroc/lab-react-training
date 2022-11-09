232e199f0613ca07671c573d6f80a45fc353895c
Sergio Rocha <sergio.rocha.pa@gmail.com>
2022-11-09 08:23:54 -0300

    Ate onde caminhei

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from "./components/IdCard"
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={"1992-07-14"}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <IdCard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={"1988-05-11"}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+
+
+ 
+    {/* Children - Tudo que é passado entre a Tag de Abertura e Fechamento do componente */}
+    <Greetings lang="de"> Ludwig</Greetings>
+    <Greetings lang="fr"> François</Greetings>
+
+  </div>
+  )
 }
 
 export default App;
---
+++ src/components/Greetings.js
@@ -0,0 +1,18 @@
+
+
+function Greetings(props) {
+    console.log(props.children)
+
+    // if (this.props.lang === 'de') => "Hallo"
+    // if (this.props.lang === 'fr') => "Bonjour"
+   
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+export default Greetings
